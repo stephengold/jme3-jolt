12018fdc5e726e1a549e4204fd6818eb779ed9f0
stephengold <sgold@sonic.net>
2026-03-03 22:26:42 -0800

    update the Jolt-JNI library to v3.8.0

--- library/src/main/java/com/github/stephengold/jme3jolt/JoltLibraryLoader.java
+++ library/src/main/java/com/github/stephengold/jme3jolt/JoltLibraryLoader.java
@@ -58,7 +58,7 @@ final public class JoltLibraryLoader {
     /**
      * expected version string of the native library
      */
-    final private static String expectedVersion = "3.6.0";
+    final private static String expectedVersion = "3.8.0";
     // *************************************************************************
     // fields
 
